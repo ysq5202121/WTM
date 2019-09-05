9cf2d916ccf39f05a0f455160b88accb74268481
LengYXin <lengyingxin8966@gmail.com>
2019-09-05 18:42:34 +0800

    test:routing test Go home and continue

--- demo/WalkingTec.Mvvm.AngularDemo/ClientApp/src/app/app-routing.module.ts
+++ demo/WalkingTec.Mvvm.AngularDemo/ClientApp/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'Monitor', loadChildren: () => import('./pages/test/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
---
+++ demo/WalkingTec.Mvvm.AngularDemo/ClientApp/src/app/pages/test/welcome.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.less']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  title = 'app';
+
+  columnDefs = [
+      {headerName: 'Make', field: 'make' },
+      {headerName: 'Model', field: 'model' },
+      {headerName: 'Price', field: 'price'}
+  ];
+
+  rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+}
